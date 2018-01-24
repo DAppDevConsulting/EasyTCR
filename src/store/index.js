bdab80cd92a7d93f8ed72befdcb4f82830c139c8
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-01-24 17:11:50 +0200

    added devtools redux extension

--- src/store/index.js
+++ src/store/index.js
@@ -5,7 +5,12 @@ import rootReducer from '../reducers';
 export default function configureStore (initialState) {
   const sagaMiddleware = createSagaMiddleware();
   return {
-    ...createStore(rootReducer, initialState, applyMiddleware(sagaMiddleware)),
+    ...createStore(
+      rootReducer,
+      initialState,
+      applyMiddleware(sagaMiddleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    ),
     runSaga: sagaMiddleware.run
   };
 }
