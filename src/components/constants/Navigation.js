35043bb80e3dabb5a5c4b5179ac7ec0ae7aa2164
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-01-24 17:13:05 +0200

    added Listing container to router

--- src/components/constants/Navigation.js
+++ src/components/constants/Navigation.js
@@ -1,4 +1,5 @@
 export const ADVERTISER = '/advertiser';
 export const PUBLISHER = '/publisher';
+export const LISTING = '/listing/:listingId';
 export const MANAGE_TOKENS = '/manage_tokens';
 export const TOKEN_HOLDER = '/token_holder';
