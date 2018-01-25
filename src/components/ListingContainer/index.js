7d537d6c06a158b0d26af1768892f6b449af897a
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-01-25 15:34:42 +0200

    mapStateToProps for listing container

--- src/components/ListingContainer/index.js
+++ src/components/ListingContainer/index.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import ListingStatus from '../ListingStatus';
 import ListingItem from '../ListingItem';
 import ListingChallenge from '../ListingChallenge';
 import './style.css';
 
-const listing = {
-  listing: 'Candidat Name',
-  description: 'The actual content will be available later',
-  // state: 'inapplication'
-  // state: 'inreveal'
-  // state: 'incommit'
-  state: 'inregistry',
-  added: '01-02-2017',
-  unchallenged: '13:04:01',
-  remaingTime: '22:12:31'
-};
+// const listing = {
+//   listing: 'Candidat Name',
+//   description: 'The actual content will be available later',
+//   // state: 'inapplication'
+//   // state: 'inreveal'
+//   // state: 'incommit'
+//   state: 'inregistry',
+//   added: '01-02-2017',
+//   unchallenged: '13:04:01',
+//   remaingTime: '22:12:31'
+// };
 
 class ListingContainer extends Component {
   challengeListing () {
@@ -22,15 +24,19 @@ class ListingContainer extends Component {
   }
 
   render () {
+    const { listing } = this.props;
+
     return (
       <div className='ContentContainer'>
-        <ListingStatus />
+        <ListingStatus
+          status={listing.status}
+        />
         <div className='ListingContainer'>
           <ListingItem
             listing={listing}
           />
           <ListingChallenge
-            remainingTime={listing.remaingTime}
+            dueDate={listing.dueDate}
             challengeHandler={this.challengeListing}
           />
         </div>
@@ -39,4 +45,25 @@ class ListingContainer extends Component {
   }
 }
 
-export default ListingContainer;
+ListingItem.propTypes = {
+  listing: PropTypes.object.isRequired
+};
+
+function mapStateToProps (state) {
+  const pathname = window.location.pathname.split('/')[2];
+
+  return {
+    listing: state.advertiser.listings.find(x => x.name === pathname),
+    minDeposit: state.parameterizer.minDeposit
+  };
+}
+
+// function mapDispatchToProps (dispatch) {
+//   return {
+//     appActions: bindActionCreators(appActions, dispatch),
+//     publisherActions: bindActionCreators(publisherActions, dispatch),
+//     advertiserActions: bindActionCreators(advertiserActions, dispatch)
+//   };
+// }
+
+export default connect(mapStateToProps)(ListingContainer);
