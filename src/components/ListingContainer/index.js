399ac9321bb1209d6ae12def98430127e4f3a5a2
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-01-25 13:14:23 +0200

    listing container

---
+++ src/components/ListingContainer/index.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import ListingStatus from '../ListingStatus';
+import ListingItem from '../ListingItem';
+import ListingChallenge from '../ListingChallenge';
+import './style.css';
+
+const listing = {
+  listing: 'Candidat Name',
+  description: 'The actual content will be available later',
+  // state: 'inapplication'
+  // state: 'inreveal'
+  // state: 'incommit'
+  state: 'inregistry',
+  added: '01-02-2017',
+  unchallenged: '13:04:01',
+  remaingTime: '22:12:31'
+};
+
+class ListingContainer extends Component {
+  challengeListing () {
+    console.log('challenge');
+  }
+
+  render () {
+    return (
+      <div className='ContentContainer'>
+        <ListingStatus />
+        <div className='ListingContainer'>
+          <ListingItem
+            listing={listing}
+          />
+          <ListingChallenge
+            listing={listing}
+            challengeHandler={this.challengeListing}
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ListingContainer;
