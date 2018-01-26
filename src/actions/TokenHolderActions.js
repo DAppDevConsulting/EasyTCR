81262d97329f5a127d8babb69979e9e13aedef62
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-01-26 14:56:48 +0200

    removed deposit

--- src/actions/TokenHolderActions.js
+++ src/actions/TokenHolderActions.js
@@ -1,8 +1,7 @@
-export function challenge (listing, deposit) {
+export function challenge (listing) {
   console.log('CHALLENGE action');
   return {
     type: 'CHALLENGE',
-    listing,
-    deposit
+    listing
   };
 }
