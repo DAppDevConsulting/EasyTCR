4a3b6edcaa1768e3a0e74229a8b49c8b1c41e8e3
Alexey Hodkov <alexey.hodkov@gmail.com>
2018-01-30 14:16:26 +0200

    fix stupid

--- src/services/ListingsMapper.js
+++ src/services/ListingsMapper.js
@@ -1,100 +1,47 @@
 import keys from '../i18n';
 
 export default class ListingsMapper {
-  static getWl (listing) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        let r = await listing.isWhitelisted();
-        resolve(r);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
-
-  static getEx (listing) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        let r = await listing.exists();
-        resolve(r);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
-
-  static getExp (listing) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        let r = await listing.expiresAt();
-        resolve(r);
-      } catch (err) {
-        reject(err);
-      }
-    });
-  }
-
   static async getProps (domain, registry) {
     let listing = registry.getListing(domain);
-    let result = {};
-    result.name = listing.name;
-    let whitelisted = false;//await listing.isWhitelisted();
-    let exists = await listing.exists();
-    console.log(exists);
-    result.status = whitelisted ? keys.inRegistry : keys.inApplication;
-    if (!exists) {
-      result.status = keys.notExists;
-    }
-    result.dueDate = '';
-    if (!whitelisted && exists) {
-      let expTs = await listing.expiresAt();
-      result.dueDate = new Date(expTs * 1000).toDateString();
-    }
-    return result;
 
-    /*try {
-      let result = await Promise.all([
+    try {
+      let props = await Promise.all([
         listing.isWhitelisted(),
         listing.exists(),
         listing.expiresAt()
       ]);
-      return result;
-    } catch (err) {
-      console.log(err);
-    }
-    return {};*/
-  }
-  static async mapListings (domains, registry) {
-    let listings = [];
-    console.time('listings');
-    try {
-      let tcrListings = await Promise.all(domains.map(async (domain) => {
-        let res = await this.getProps(domain, registry)
-        console.log(res);
-        return res;
-      }));
-      console.log(tcrListings);
-    } catch (err) {
-      console.log(err);
-    }
-    console.timeEnd('listings');
-    for (let domain of domains) {
-      let listing = registry.getListing(domain.listing);
-      let result = {};
-      result.name = listing.name;
-      let whitelisted = await listing.isWhitelisted();
-      let exists = await listing.exists();
+      let whitelisted = props[0];
+      let exists = props[1];
+      let expTs = props[2];
+
+      let result = {name: listing.name};
       result.status = whitelisted ? keys.inRegistry : keys.inApplication;
       if (!exists) {
         result.status = keys.notExists;
       }
       result.dueDate = '';
       if (!whitelisted && exists) {
-        let expTs = await listing.expiresAt();
         result.dueDate = new Date(expTs * 1000).toDateString();
       }
-      listings.push(result);
+
+      return result;
+    } catch (err) {
+      console.log(err);
     }
-    return listings;
+    return {};
+  }
+  static async mapListings (domains, registry) {
+    console.time('listings');
+    try {
+      let tcrListings = await Promise.all(domains.map(async (domain) => {
+        let res = await this.getProps(domain.listing, registry);
+        return res;
+      }));
+      console.timeEnd('listings');
+      return tcrListings;
+    } catch (err) {
+      console.log(err);
+      return [];
+    }
   }
 }
