55b740e558451455ef0b533df3889014b805089f
Dmytrii Kulbaka <kulbaka.dmytrii@gmail.com>
2018-01-30 15:17:02 +0200

    fixed challenge tx visibility issue

--- src/actions/TokenHolderActions.js
+++ src/actions/TokenHolderActions.js
@@ -1,8 +1,14 @@
-import { CHALLENGE_START } from '../constants/actions';
+import { CHALLENGE_START, CHALLENGE_HIDE_TX_QUEUE } from '../constants/actions';
 
 export function challenge (listing) {
   return {
     type: CHALLENGE_START,
     listing
+  };
+}
+
+export function hideTxQueue () {
+  return {
+    type: CHALLENGE_HIDE_TX_QUEUE
   };
 }
