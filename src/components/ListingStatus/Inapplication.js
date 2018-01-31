06d1b5ca5b528598cbf5664436a4cdb7d381e731
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-01-31 12:05:57 +0200

    candidate statuses

---
+++ src/components/ListingStatus/Inapplication.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const Inapplication = () =>
+  <svg width='422' height='64' viewBox='0 0 422 64' xmlns='http://www.w3.org/2000/svg'>
+    <g transform='translate(0 -4)' fill='none' fillRule='evenodd'>
+      <text fontFamily='OpenSans-Semibold, Open Sans' fontSize='12' fontWeight='500' fill='#7F8FA4'>
+        <tspan x='18.03' y='13.85'>In application</tspan>
+      </text>
+      <text opacity='.3' fontFamily='OpenSans, Open Sans' fontSize='12' fill='#7F8FA4'>
+        <tspan x='132.52' y='13.14'>Voting commit</tspan>
+      </text>
+      <text opacity='.3' fontFamily='OpenSans, Open Sans' fontSize='12' fill='#7F8FA4'>
+        <tspan x='251.35' y='13.14'>Voting revial</tspan>
+      </text>
+      <text opacity='.3' fontFamily='OpenSans, Open Sans' fontSize='12' fill='#7F8FA4'>
+        <tspan x='366.17' y='13.14'>In registry</tspan>
+      </text>
+      <text opacity='.3' fontFamily='OpenSans, Open Sans' fontSize='12' fill='#7F8FA4'>
+        <tspan x='134.67' y='64.73'>In registry</tspan>
+      </text>
+      <text opacity='.3' fontFamily='OpenSans, Open Sans' fontSize='12' fill='#7F8FA4'>
+        <tspan x='366.06' y='64.73'>Rejected</tspan>
+      </text>
+      <circle fill='#CAC7CB' cx='13.19' cy='35.36' r='3.79' />
+      <path d='M12.79 35.16l353.25-.02' stroke='#7F8FA4' strokeLinecap='square' />
+      <circle stroke='#7F8FA4' fill='#7F8FA4' cx='10.48' cy='10.58' r='3.5' />
+      <path d='M10.45 35.01V7.81' stroke='#7F8FA4' strokeLinecap='square' />
+      <g transform='translate(.48 24.37)'>
+        <ellipse stroke='#7F8FA4' fill='#8BC34A' cx='10.03' cy='9.84' rx='9.88' ry='9.5' />
+        <path d='M2.83 2.92h14.4v13.85H2.83z' />
+        <path fill='#FFF' fillRule='nonzero' d='M8.23 12.25l-2.5-2.4-.85.8 3.35 3.23 7.2-6.92-.84-.82z' />
+      </g>
+      <path d='M124.93 56.7V35.8h1v20.88a3.5 3.5 0 1 1-1 .01zM124 14.04a3.5 3.5 0 1 1 1 0v20.97h-1V14.04z' fill='#7F8FA4' opacity='.3' />
+      <g transform='translate(114.54 24.37)'>
+        <ellipse strokeOpacity='.3' stroke='#7F8FA4' fill='#FFF' cx='10.03' cy='9.84' rx='9.88' ry='9.5' />
+        <text opacity='.3' fontFamily='OpenSans, Open Sans' fontSize='12' fill='#7F8FA4'>
+          <tspan x='6.6' y='14.35'>2</tspan>
+        </text>
+      </g>
+      <path d='M240.04 14.04a3.5 3.5 0 1 1 1 0v20.97h-1V14.04z' fill='#7F8FA4' opacity='.3' />
+      <ellipse cx='10.03' cy='9.84' rx='9.88' ry='9.5' transform='translate(230.57 24.37)' fill='#FFF' stroke='#7F8FA4' strokeOpacity='.3' />
+      <text opacity='.3' fontFamily='OpenSans, Open Sans' fontSize='12' fill='#7F8FA4'>
+        <tspan x='237.11' y='38.63'>3</tspan>
+      </text>
+      <path d='M355.95 14.04a3.5 3.5 0 1 1 1 0v20.97h-1V14.04zM355.93 56.7V35.8h1v20.88a3.5 3.5 0 1 1-1 .01z' fill='#7F8FA4' opacity='.3' />
+      <ellipse cx='10.03' cy='9.84' rx='9.88' ry='9.5' transform='translate(346.48 24.37)' fill='#FFF' stroke='#7F8FA4' strokeOpacity='.3' />
+      <text opacity='.3' fontFamily='OpenSans, Open Sans' fontSize='12' fill='#7F8FA4'>
+        <tspan x='353.11' y='38.63'>4</tspan>
+      </text>
+    </g>
+  </svg>;
+
+export default Inapplication;
