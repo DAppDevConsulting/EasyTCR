82afe52b9d316fd2cee93dd866406c6f59d1799d
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-02-01 12:07:18 +0200

    manage tokens style

--- src/components/ManageTokensContainer/index.js
+++ src/components/ManageTokensContainer/index.js
@@ -34,30 +34,40 @@ class ManageTokensContainer extends Component {
       keys.manageTokensPage_balanceText,
       {tokens, tokenName: keys.tokenName, ethers, eth: keys.eth}
     );
+    const labelText = keys.formatString(
+      keys.manageTokensPage_rate,
+      {price: this.state.price, wei: keys.wei, tokenName: keys.tokenName}
+    );
     return (
       <div className='ContentContainer'>
-        <div>{keys.manageTokensPage_title}</div>
-        <h3> {keys.manageTokensPage_balanceHeader} </h3>
-        <div>{fetching ? 'Updating...' : balanceText}</div>
-        <h3> {keys.manageTokensPage_buyTokensHeader} </h3>
-        <div>{keys.formatString(keys.manageTokensPage_rate, {price: this.state.price, wei: keys.wei, tokenName: keys.tokenName})}</div>
+        <h4 className='sectionTitle'>{keys.manageTokensPage_title}</h4>
+        <h3 className='manageTokensTitle'> {keys.manageTokensPage_balanceHeader} </h3>
+        <p className='balanceText'>{tokens ? balanceText : 'Updating...' }</p>
+        <h3 className='manageTokensTitle'> {keys.manageTokensPage_buyTokensHeader} </h3>
         <div className='buyTokensForm'>
           <div className='buyTokensForm_item'>
             <div className='buyTokensForm_element'>
               <TextField
+                floatingLabelText={labelText}
+                floatingLabelFixed
                 hintText={keys.manageTokensPage_buyTokensHint}
                 value={this.state.value || ''}
                 onChange={(e, value) => this.setState({ value: new BN(value, 10) })} />
             </div>
             <div className='buyTokensForm_element'>
-              <RaisedButton label={keys.buy} disabled={!this.state.value} onClick={() => {
-                this.buyTokens();
-              }} />
+              <RaisedButton
+                label={keys.buy}
+                disabled={!this.state.value}
+                onClick={() => this.buyTokens()}
+                backgroundColor='#66bb6a'
+                labelColor='#fff'
+                style={{ marginBottom: '8px' }}
+              />
             </div>
           </div>
         </div>
-        <div>{keys.formatString(keys.manageTokensPage_supposedTokens, this.getTokensToBuy().toString(), keys.tokenName)}</div>
-        <div>{keys.formatString(keys.manageTokensPage_supposedPrice, this.getTotalPriceText())}</div>
+        {/* <div>{keys.formatString(keys.manageTokensPage_supposedTokens, this.getTokensToBuy().toString(), keys.tokenName)}</div> */}
+        <p className='balanceText'>{keys.formatString(keys.manageTokensPage_supposedPrice, this.getTotalPriceText())}</p>
       </div>
     );
   }
