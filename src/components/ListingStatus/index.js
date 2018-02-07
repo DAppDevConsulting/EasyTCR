b826e43ddfa8f7ee950186ba47085d936c337d3c
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-02-07 17:50:23 +0200

    need refresh status added

--- src/components/ListingStatus/index.js
+++ src/components/ListingStatus/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import RaisedButton from 'material-ui/RaisedButton';
+import FontIcon from 'material-ui/FontIcon';
 import './style.css';
 // todo: make one status component and pass props
 import Inapplication from './Inapplication';
@@ -7,6 +9,7 @@ import Inreveal from './Inreveal';
 import Incommit from './Incommit';
 import Inregistry from './Inregistry';
 // import InregistryAfter from './InregistryAfter';
+import NeedRefresh from './NeedRefresh';
 import Rejected from './Rejected';
 import keys from '../../i18n';
 
@@ -22,9 +25,12 @@ const renderStatus = status => {
     //   return <Rejected />;
     case keys.inApplication:
       return <Inapplication />;
+    case keys.NeedRefresh:
+      return <NeedRefresh />;
     // in application
     default:
       return null;
+      // return <Inreveal />;
   }
 };
 
@@ -32,6 +38,19 @@ const ListingStatus = ({ status }) => (
   <div className='listingStatus'>
     <p>Status:</p>
     { renderStatus(status)}
+    { status === 'Need refresh'
+      ? <div className='refreshStatus'>
+          <RaisedButton
+            label='Refresh'
+            buttonStyle={{ backgroundColor: 'rgba(153, 153, 153, 0.2)' }}
+            style={{ marginLeft: '20px' }}
+            labelStyle={{ textTransform: 'Capitalize', color: '#3f536e' }}
+            icon={<FontIcon className='fa fa-spinner fa-spin' />}
+          />
+          <p>To update candidate’s status in TCR proceed with refresh transaction.</p>
+        </div>
+      : null
+    }
   </div>
 );
 
