2458cd2528e7390c834020d9e505fbf7103cab71
Alexey Hodkov <alexey.hodkov@gmail.com>
2018-02-14 16:05:55 +0200

    fix reveal contract exception

--- src/sagas/commit.js
+++ src/sagas/commit.js
@@ -1,11 +1,12 @@
 import { takeEvery, put, call } from 'redux-saga/effects';
 import { PLCRVoting } from 'ethereum-tcr-api';
+import BN from 'bn.js';
 import { commitVote as getCommitVoteTx } from '../transactions';
 import { COMMIT_SHOW_TX_QUEUE, COMMIT_SEND } from '../constants/actions';
 
 export function * commitVote (action) {
   // {id: Number, salt: Number, option: Number, stake: Number}
-  let hash = PLCRVoting.makeSecretHash(action.option, action.salt);
+  let hash = PLCRVoting.makeSecretHash(action.option, new BN(action.salt, 16));
   let queue = yield call(getCommitVoteTx, action.id, hash, action.stake);
 
   yield put({ type: COMMIT_SHOW_TX_QUEUE, queue });
--- src/sagas/reveal.js
+++ src/sagas/reveal.js
@@ -1,10 +1,11 @@
 import { takeEvery, put, call } from 'redux-saga/effects';
+import BN from 'bn.js';
 import { revealVote as getRevealVoteTx } from '../transactions';
 import { REVEAL_SHOW_TX_QUEUE, REVEAL_SEND } from '../constants/actions';
 
 export function * revealVote (action) {
   // action: {id: Number, salt: Number, option: Number}
-  let queue = yield call(getRevealVoteTx, action.id, action.option, action.salt);
+  let queue = yield call(getRevealVoteTx, action.id, action.option, new BN(action.salt, 16));
 
   yield put({ type: REVEAL_SHOW_TX_QUEUE, queue });
 }
