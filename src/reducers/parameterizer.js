d9518b9e43e1b192fbd1ee7e30e57c9cb36288f8
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-02-14 15:32:13 +0200

    parameterizer state

--- src/reducers/parameterizer.js
+++ src/reducers/parameterizer.js
@@ -1,14 +1,47 @@
-import { UPDATE_PARAMETERIZER_INFORMATION } from '../constants/actions';
+import { UPDATE_PARAMETERIZER_INFORMATION, REQUEST_PARAMETERIZER_INFORMATION } from '../constants/actions';
 
 const initialState = {
-  minDeposit: 0
+  parameters: {
+    minDeposit: {
+      value: 0,
+      status: null,
+      proposal: null,
+    },
+    applyStageLength: {
+      value: 0,
+      status: null,
+      proposal: null,
+    },
+    commitStageLength: {
+      value: 0,
+      status: null,
+      proposal: null,
+    },
+    revealStageLength: {
+      value: 0,
+      status: null,
+      proposal: null,
+    },
+    dispensationPercent: {
+      value: 0,
+      status: null,
+      proposal: null,
+    },
+    voteQuorum: {
+      value: 0,
+      status: null,
+      proposal: null,
+    },
+  },
+  isFetching: false,
 };
 
 export default function parameterizer (state = initialState, action) {
   switch (action.type) {
     case UPDATE_PARAMETERIZER_INFORMATION:
-      return {...state, ...action.params};
-
+      return {...state, parameters: action.params, isFetching: false};
+    case REQUEST_PARAMETERIZER_INFORMATION:
+      return {...state, isFetching: true};
     default:
       return state;
   }
--- src/sagas/parameterizer.js
+++ src/sagas/parameterizer.js
@@ -8,7 +8,38 @@ import {
 export function * fetchParameters (action) {
   let parameterizer = yield apply(TCR.registry(), 'getParameterizer');
   // Don't mind this shitty piece, it'll work cool when we have getParameters() method for parameterizer
-  let params = {minDeposit: yield apply(parameterizer, 'get', ['minDeposit'])};
+  let params = {
+    minDeposit: {
+      value: yield apply(parameterizer, 'get', ['minDeposit']),
+      status: 'In registry',
+      proposal: null,
+    },
+    applyStageLength: {
+      value: yield apply(parameterizer, 'get', ['applyStageLen']),
+      status: 'In registry',
+      proposal: null,
+    },
+    commitStageLength: {
+      value: yield apply(parameterizer, 'get', ['commitStageLen']),
+      status: 'In challenge',
+      proposal: null,
+    },
+    revealStageLength: {
+      value: yield apply(parameterizer, 'get', ['revealStageLen']),
+      status: 'In challenge',
+      proposal: null,
+    },
+    dispensationPercent: {
+      value: yield apply(parameterizer, 'get', ['dispensationPct']),
+      status: 'End of voting',
+      proposal: null,
+    },
+    voteQuorum: {
+      value: yield apply(parameterizer, 'get', ['voteQuorum']),
+      status: 'In registry',
+      proposal: null,
+    },
+  };
 
   yield put({type: UPDATE_PARAMETERIZER_INFORMATION, params});
 }
