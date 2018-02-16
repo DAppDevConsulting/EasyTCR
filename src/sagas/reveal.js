efd22ecd831a62dca00e556017e6af58fe6b8dd8
Alexey Hodkov <alexey.hodkov@gmail.com>
2018-02-16 12:51:16 +0200

    fix salt and hash

--- src/sagas/reveal.js
+++ src/sagas/reveal.js
@@ -5,7 +5,7 @@ import { REVEAL_SHOW_TX_QUEUE, REVEAL_SEND } from '../constants/actions';
 
 export function * revealVote (action) {
   // action: {id: Number, salt: Number, option: Number}
-  let queue = yield call(getRevealVoteTx, action.id, action.option, new BN(action.salt, 16));
+  let queue = yield call(getRevealVoteTx, action.id, action.option, action.salt);
 
   yield put({ type: REVEAL_SHOW_TX_QUEUE, queue });
 }
