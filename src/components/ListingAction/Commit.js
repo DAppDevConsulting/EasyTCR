169bdf822e7c2e5e6ffd01091baeb839fa0d6b95
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-02-22 13:54:54 +0200

    remaining time added

--- src/components/ListingAction/Commit.js
+++ src/components/ListingAction/Commit.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import moment from "moment";
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import TextField from 'material-ui/TextField';
 import { RadioButton, RadioButtonGroup } from 'material-ui/RadioButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import DownloadIcon from 'material-ui/svg-icons/file/file-download';
+import LinearProgress from "material-ui/LinearProgress";
 import randomInt from 'random-int';
 import * as tokenHolderActions from '../../actions/TokenHolderActions';
 import TxQueue from '../TxQueue';
@@ -16,13 +18,33 @@ class Commit extends Component {
     super(props);
 
     this.handleVote = this.handleVote.bind(this);
+    this.calculateRemainingTime = this.calculateRemainingTime.bind(this);
 
     this.state = {
       hasVoted: false,
       salt: randomInt(1e6, 1e8),
       stake: 1,
-      option: 1
+      option: 1,
+      remainingTime: null,
     };
+  }
+
+  componentDidMount () {
+    this.setState({
+      intervalObj: setInterval(() => this.calculateRemainingTime(), 1000)
+    });
+  }
+
+  componentWillUnmount () {
+    this.props.tokenHolderActions.hideTxQueue();
+    clearInterval(this.state.intervalObj);
+  }
+
+  calculateRemainingTime () {
+    let diff = moment.duration(this.props.listing.timestamp - moment().valueOf());
+    this.setState({
+      remainingTime: diff > 0 ? diff.humanize() : 'passed'
+    });
   }
 
   handleVote () {
@@ -50,10 +72,19 @@ class Commit extends Component {
 
   renderVoteForm () {
     const { listing } = this.props;
+    const { remainingTime } = this.state;
     return (
       <div>
-        <h4 className='headline'>{keys.commitStage}</h4>
+        <h4 className='actionTitle'>{keys.commitStage}</h4>
         <div className='actionData'>
+          <div className='challengeTime'>
+            <p>{keys.remainingTimeText}</p>
+            {
+              remainingTime
+                ? <p>{remainingTime}</p>
+                : <LinearProgress mode='indeterminate' style={{ width: '100px', marginTop: '7px' }} />
+            }
+          </div>
           { listing ? <p className='challengeId'>{keys.challengeIdText}: {listing.challengeId}</p> : null }
           <TextField
             floatingLabelText={keys.enterVotes}
