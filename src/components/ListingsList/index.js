4b0df710ee9bf058c5ad799dd4963ddff870e2b6
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-02-22 10:56:25 +0200

    list of listings refactoring

--- src/components/ListingsList/index.js
+++ src/components/ListingsList/index.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import {
   Table,
@@ -11,83 +11,72 @@ import {
 import PropTypes from 'prop-types';
 import keys from '../../i18n';
 
-class ListingsList extends Component {
-  renderHeader (config, adjustForCheckbox, selectable) {
-    return (
-      <TableHeader adjustForCheckbox={adjustForCheckbox} displaySelectAll={selectable}>
-        <TableRow>
-          {config.columns.map((column, index) => (
-            <TableHeaderColumn key={index}>{column.title}</TableHeaderColumn>
-          ))}
-        </TableRow>
-      </TableHeader>
-    );
-  }
+const renderRow = (config, data, index) => (
+  <TableRow key={index}>
+    {config.columns.map(column => {
+      const key = `${index}_${column.propName}`;
 
-  renderRow (config, data, index) {
-    return (
-      <TableRow key={index}>
-        {config.columns.map((column) => {
-          const key = `${index}_${column.propName}`;
+      if (column.propName === "status") {
+        // check for WillBeWhitelisted and WillBeRejected statuses and render Need refresh
+        return (
+          <TableRowColumn key={key}>
+            {data[column.propName] === keys.WillBeWhitelisted ||
+            data[column.propName] === keys.WillBeRejected
+              ? keys.needRefresh
+              : data[column.propName]}
+          </TableRowColumn>
+        );
+      } else if (column.propName === "action") {
+        // TODO: view valid action state
+        return (
+          <TableRowColumn key={key}>
+            <Link
+              to={`candidate/${data.name}`}
+              style={{
+                color: key.accentColor,
+                fontWeight: "400",
+                textTransform: "uppercase"
+              }}
+            >View
+            </Link>
+          </TableRowColumn>
+        );
+      }
 
-          if (column.propName === 'status') {
-            return (
-              <TableRowColumn key={key}>{
-                data[column.propName] === keys.WillBeWhitelisted || data[column.propName] === keys.WillBeRejected
-                ? keys.needRefresh
-                : data[column.propName]
-              }</TableRowColumn>
-            );
-          } else if (column.propName !== 'action') {
-            return (<TableRowColumn key={key}>{data[column.propName]}</TableRowColumn>);
-          } 
+      return (
+        <TableRowColumn key={key}>{data[column.propName]}</TableRowColumn>
+      );
+    })}
+  </TableRow>
+);
 
-          // TODO: view valid action state
-          return (
-            <TableRowColumn key={key}>
-              <Link
-                to={`candidate/${data.name}`}
-                style={{ color: '#748ffc', fontWeight: '400', textTransform: 'uppercase' }}
-              >View
-              </Link>
-            </TableRowColumn>
-          );
-        })}
-      </TableRow>
-    );
-  }
-
-  render () {
-    const {listings, config} = this.props;
-
-    const selectable = false;
-    const adjustForCheckbox = false;
-
-    return (
-      <div>
-        <Table
-          fixedHeader
-          fixedFooter
-          selectable={selectable}
+const ListingsList = ({ listings, config }) => {
+  return (
+    <div>
+      <Table fixedHeader fixedFooter selectable={false}>
+        <TableHeader adjustForCheckbox={false} displaySelectAll={false}>
+          <TableRow>
+            {config.columns.map((column, index) => (
+              <TableHeaderColumn key={index}>{column.title}</TableHeaderColumn>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody
+          displayRowCheckbox={false}
+          deselectOnClickaway
+          showRowHover
+          stripedRows={false}
         >
-          {this.renderHeader(config, adjustForCheckbox, selectable)}
-          <TableBody
-            displayRowCheckbox={false}
-            deselectOnClickaway
-            showRowHover
-            stripedRows={false}
-          >
-            { listings.map((row, index) => this.renderRow(config, row, index)).reverse() }
-          </TableBody>
-        </Table>
-      </div>
-    );
-  }
-}
+          {listings.reverse().map((row, index) => renderRow(config, row, index))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
 
 ListingsList.propTypes = {
   listings: PropTypes.array.isRequired,
-  config: PropTypes.object.isRequired
+  config: PropTypes.object.isRequired,
 };
 
 export default ListingsList;
