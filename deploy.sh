ff5191cebda1d82b7d256727da039ebe34b3979c
Rostyslav Golda <tx_hv@ukr.net>
2018-02-27 02:09:17 +0200

    Revert last commit

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 docker stop ethereum-tcr-ui-deploy-$1
 docker rm ethereum-tcr-ui-deploy-$1
 cat Dockerfile-template | sed s/_DIR/$1/ | sed s/_PORT/$(cat ~gitlab-runner/tcr-ui-apps/$1/port)/ > Dockerfile-$1
-cp -rf ~gitlab-runner/tcr-ui-apps/$1/* .
+cp -rf ~gitlab-runner/tcr-ui-apps/* .
 docker build -f Dockerfile-$1 . -t ethereum-tcr-ui-deploy-$1
 docker run -d --name ethereum-tcr-ui-deploy-$1 --network host -t ethereum-tcr-ui-deploy-$1
