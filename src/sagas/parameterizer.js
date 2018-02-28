3bc27709d31522d536e24a6a41a5afcabb5cb22e
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-02-28 15:49:25 +0200

    parameterizer saga - getProposalValue

--- src/sagas/parameterizer.js
+++ src/sagas/parameterizer.js
@@ -10,57 +10,67 @@ import api from '../services/BackendApi'
 import { proposeNewParameterizerValue as getProposeNewParameterizerValue } from '../transactions'
 import keys from '../i18n';
 
+const getProposalValue = (proposals, parameterName) =>
+	proposals.find(x => x.name === parameterName)
+		? proposals.find(x => x.name === parameterName).value
+		: null
+
 export function * fetchParameters (action) {
-  let parameterizer = yield apply(TCR.registry(), 'getParameterizer');
+  const parameterizer = yield apply(TCR.registry(), 'getParameterizer');
 
-  // let proposals = yield apply(
-  //   api,
-  //   'getParameterizerProposals',
-  //   [parameterizer.address, TCR.defaultAccountAddress()]
-  // )
+  const proposalEvents = yield apply(
+    api,
+    'getParameterizerProposals',
+    [parameterizer.address, TCR.defaultAccountAddress()]
+  )
 
-  let params = [
+  const proposals = proposalEvents.map(p => ({
+		name: p.returnValues.name,
+		value: p.returnValues.value,
+  }))
+
+  const params = [
     {
       displayName: keys.tableParameterNames[0],
       contractName: keys.contractParameterNames[0],
       value: yield apply(parameterizer, 'get', [keys.contractParameterNames[0]]),
       status: keys.inRegistry,
-      proposal: null,
+      proposal: getProposalValue(proposals, keys.contractParameterNames[0])
     },
     {
       displayName: keys.tableParameterNames[1],
       contractName: keys.contractParameterNames[1],
       value: yield apply(parameterizer, 'get', [keys.contractParameterNames[1]]),
       status: keys.inRegistry,
-      proposal: null,
+      proposal: getProposalValue(proposals, keys.contractParameterNames[1])
     },
     {
       displayName: keys.tableParameterNames[2],
       contractName: keys.contractParameterNames[2],
       value: yield apply(parameterizer, 'get', [keys.contractParameterNames[2]]),
       status: keys.inRegistry,
-      proposal: null,
+      proposal: getProposalValue(proposals, keys.contractParameterNames[2])
     },
     {
       displayName: keys.tableParameterNames[3],
       contractName: keys.contractParameterNames[3],
       value: yield apply(parameterizer, 'get', [keys.contractParameterNames[3]]),
       status: keys.inRegistry,
-      proposal: null,
+      proposal: getProposalValue(proposals, keys.contractParameterNames[3])
     },
     {
       displayName: keys.tableParameterNames[4],
       contractName: keys.contractParameterNames[4],
       value: yield apply(parameterizer, 'get', [keys.contractParameterNames[4]]),
       status: keys.inRegistry,
-      proposal: null,
+      proposal: getProposalValue(proposals, keys.contractParameterNames[4])
     },
     {
       displayName: keys.tableParameterNames[5],
       contractName: keys.contractParameterNames[5],
       value: yield apply(parameterizer, 'get', [keys.contractParameterNames[5]]),
       status: keys.inRegistry,
-      proposal: null,
+      proposal: getProposalValue(proposals, keys.contractParameterNames[5])
     },
   ];
 
