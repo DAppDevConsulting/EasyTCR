434220e4401b2917a78d95e67f03fbada5bb8bdf
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-02-28 14:19:07 +0200

    parameterizer reducer

--- src/reducers/parameterizer.js
+++ src/reducers/parameterizer.js
@@ -1,55 +1,71 @@
-import { UPDATE_PARAMETERIZER_INFORMATION, REQUEST_PARAMETERIZER_INFORMATION } from '../constants/actions';
+import {
+	UPDATE_PARAMETERIZER_INFORMATION,
+	REQUEST_PARAMETERIZER_INFORMATION,
+	PARAMETERIZER_SHOW_TX_QUEUE,
+	PARAMETERIZER_HIDE_TX_QUEUE,
+} from '../constants/actions'
 import keys from '../i18n';
 
 const initialState = {
-  parameters: [
-    {
-      name: keys.tableParameterNames[0],
-      value: 0,
-      status: null,
-      proposal: null,
-    },
-    {
-      name: keys.tableParameterNames[1],
-      value: 0,
-      status: null,
-      proposal: null,
-    },
-    {
-      name: keys.tableParameterNames[2],
-      value: 0,
-      status: null,
-      proposal: null,
-    },
-    {
-      name: keys.tableParameterNames[3],
-      value: 0,
-      status: null,
-      proposal: null,
-    },
-    {
-      name: keys.tableParameterNames[4],
-      value: 0,
-      status: null,
-      proposal: null,
-    },
-    {
-      name: keys.tableParameterNames[5],
-      value: 0,
-      status: null,
-      proposal: null,
-    },
-  ],
-  isFetching: false,
-};
+	parameters: [
+		{
+			displayName: keys.tableParameterNames[0],
+			contractName: keys.contractParameterNames[0],
+			value: 0,
+			status: null,
+			proposal: null,
+		},
+		{
+			displayName: keys.tableParameterNames[1],
+			contractName: keys.contractParameterNames[1],
+			value: 0,
+			status: null,
+			proposal: null,
+		},
+		{
+			displayName: keys.tableParameterNames[2],
+			contractName: keys.contractParameterNames[2],
+			value: 0,
+			status: null,
+			proposal: null,
+		},
+		{
+			displayName: keys.tableParameterNames[3],
+			contractName: keys.contractParameterNames[3],
+			value: 0,
+			status: null,
+			proposal: null,
+		},
+		{
+			displayName: keys.tableParameterNames[4],
+			contractName: keys.contractParameterNames[4],
+			value: 0,
+			status: null,
+			proposal: null,
+		},
+		{
+			displayName: keys.tableParameterNames[5],
+			contractName: keys.contractParameterNames[5],
+			value: 0,
+			status: null,
+			proposal: null,
+		},
+	],
+	isFetching: false,
+	showTxQueue: false,
+	queue: null,
+}
 
 export default function parameterizer (state = initialState, action) {
   switch (action.type) {
-    case UPDATE_PARAMETERIZER_INFORMATION:
-      return {...state, parameters: action.params, isFetching: false};
-    case REQUEST_PARAMETERIZER_INFORMATION:
-      return {...state, isFetching: true};
-    default:
-      return state;
-  }
+		case UPDATE_PARAMETERIZER_INFORMATION:
+			return { ...state, parameters: action.params, isFetching: false, showTxQueue: false }
+		case REQUEST_PARAMETERIZER_INFORMATION:
+			return { ...state, isFetching: true }
+		case PARAMETERIZER_SHOW_TX_QUEUE:
+			return { ...state, queue: action.queue, showTxQueue: true }
+		case PARAMETERIZER_HIDE_TX_QUEUE:
+		default:
+			return state
+	}
 }
