5b3737d7c2ca5a3fbc6ef768b6e2e5ee96291d45
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-02-28 14:30:13 +0200

    getParameterizerProposals method in api

--- src/services/BackendlessApi.js
+++ src/services/BackendlessApi.js
@@ -9,40 +9,64 @@ const TCRofTCRs = {
 const registriesCache = new Map();
 
 export default {
-  getRegistries: async () => {
-    registriesCache.clear();
-    let listings = await contractService.getListings(TCRofTCRs.registry);
-    let registries = await Promise.all(
-      // TODO: filter to remove mistake in contract. remove this shit.
-      listings.filter(item => item.listing !== 'QmWqzxG4aff4htBi4sEeQiK42L8oPTRE6cbk44HRxgJvLZ')
-        .map(item => IPFS.get(item.listing))
-    );
-    registries.forEach(item => registriesCache.set(item.id, item));
-    return registries.map(item => {
-      item.registry = item.id;
-      return item;
-    }).sort((a, b) => {
-      return a.id > b.id ? 1 : -1;
-    });
-  },
-  getRegistryLocalization: async (registry) => {
-    return registriesCache.has(registry) ? registriesCache.get(registry).localization : '';
-  },
-  getListings: async (registry, account, filters = [], address = '') => {
-    let listings = await contractService.getListings(registry, account);
-    if (address) {
-      return listings.filter((item) => item.account === address);
-    }
-    return listings;
-  },
-  getListingsToClaimReward: async (registry, account) => {
-    let listings = await contractService.getListingsToClaimReward(registry, account);
-    return listings;
-  },
-  listenNotification: (handler) => {
-    contractService.setRegistryNotificationHandler(handler);
-  },
-  listenRewordsNotification: (handler) => {
-    contractService.setRewardNotificationHandler(handler);
-  }
-};
+	getRegistries: async () => {
+		registriesCache.clear()
+		let listings = await contractService.getListings(TCRofTCRs.registry)
+		let registries = await Promise.all(
+			// TODO: filter to remove mistake in contract. remove this shit.
+			listings
+				.filter(
+					item =>
+						item.listing !==
+						'QmWqzxG4aff4htBi4sEeQiK42L8oPTRE6cbk44HRxgJvLZ',
+				)
+				.map(item => IPFS.get(item.listing)),
+		)
+		registries.forEach(item => registriesCache.set(item.id, item))
+		return registries
+			.map(item => {
+				item.registry = item.id
+				return item
+			})
+			.sort((a, b) => {
+				return a.id > b.id ? 1 : -1
+			})
+	},
+	getRegistryLocalization: async registry => {
+		return registriesCache.has(registry)
+			? registriesCache.get(registry).localization
+			: ''
+	},
+	getListings: async (registry, account, filters = [], address = '') => {
+		let listings = await contractService.getListings(registry, account)
+		if (address) {
+			return listings.filter(item => item.account === address)
+		}
+		return listings
+	},
+	getListingsToClaimReward: async (registry, account) => {
+		let listings = await contractService.getListingsToClaimReward(
+			registry,
+			account,
+		)
+		return listings
+	},
+	getParameterizerProposals: async (registry, account) => {
+		console.log(
+			'getParameterizerProposals in backendless api',
+			registry,
+			account,
+		)
+		// let parameterizerProposals = await contractService.getParameterizerProposals(
+		// 	registry,
+		// 	account,
+		// )
+		// return parameterizerProposals
+	},
+	listenNotification: handler => {
+		contractService.setRegistryNotificationHandler(handler)
+	},
+	listenRewordsNotification: handler => {
+		contractService.setRewardNotificationHandler(handler)
+	},
+}
