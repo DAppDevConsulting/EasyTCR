8ff416195eaef239e374d949276857e5ff3af4cd
Rostyslav Golda <tx_hv@ukr.net>
2018-02-28 23:23:57 +0200

    Removed build. Added faster deploy without service down gaps.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-docker stop ethereum-tcr-ui-deploy-$1
-docker rm ethereum-tcr-ui-deploy-$1
-cat Dockerfile-template | sed s/_DIR/$1/ | sed s/_PORT/$(cat ~gitlab-runner/tcr-ui-apps/$1/port)/ > Dockerfile-$1
+COUNTER=0
+
+if [ -e ~gitlab-runner/tcr-ui-apps/$1/counter ]
+then
+    COUNTER=$(cat ~gitlab-runner/tcr-ui-apps/$1/counter)
+fi
+
+INC=`expr $COUNTER '+' 1`
+cat Dockerfile-template | sed s/_DIR/$1/ | sed s/_PORT/$(cat ~gitlab-runner/tcr-ui-apps/"$1"/port)/ > Dockerfile-$1
 cp -rf ~gitlab-runner/tcr-ui-apps/* .
-docker build -f Dockerfile-$1 . -t ethereum-tcr-ui-deploy-$1
-docker run -d --name ethereum-tcr-ui-deploy-$1 --network host -t ethereum-tcr-ui-deploy-$1
+docker build -f Dockerfile-$1 . -t ethereum-tcr-ui-deploy-"$1"-$INC
+docker stop ethereum-tcr-ui-deploy-"$1"-$COUNTER
+docker rm ethereum-tcr-ui-deploy-"$1"-$COUNTER
+docker run -d --name ethereum-tcr-ui-deploy-"$1"-$INC --network host -t ethereum-tcr-ui-deploy-"$1"-$INC
+
+echo "$INC" > ~gitlab-runner/tcr-ui-apps/$1/counter
