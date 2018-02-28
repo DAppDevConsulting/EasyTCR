cc6c3bd683d0b89e52017eafe808f1d055fed94c
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-02-28 18:03:17 +0200

    get proposal state at parameterizer

--- src/sagas/parameterizer.js
+++ src/sagas/parameterizer.js
@@ -1,4 +1,4 @@
-import { call, put, takeEvery, apply } from 'redux-saga/effects'
+import { all, call, put, takeEvery, apply } from 'redux-saga/effects'
 import TCR from '../TCR';
 import {
 	UPDATE_PARAMETERIZER_INFORMATION,
@@ -10,10 +10,37 @@ import api from '../services/BackendApi'
 import { proposeNewParameterizerValue as getProposeNewParameterizerValue } from '../transactions'
 import keys from '../i18n';
 
+const paramsList = [
+	{
+		displayName: keys.tableParameterNames[0],
+		contractName: keys.contractParameterNames[0],
+	},
+	{
+		displayName: keys.tableParameterNames[1],
+		contractName: keys.contractParameterNames[1],
+	},
+	{
+		displayName: keys.tableParameterNames[2],
+		contractName: keys.contractParameterNames[2],
+	},
+	{
+		displayName: keys.tableParameterNames[3],
+		contractName: keys.contractParameterNames[3],
+	},
+	{
+		displayName: keys.tableParameterNames[4],
+		contractName: keys.contractParameterNames[4],
+	},
+	{
+		displayName: keys.tableParameterNames[5],
+		contractName: keys.contractParameterNames[5],
+	},
+]
+
 const getProposalValue = (proposals, parameterName) =>
 	proposals.find(x => x.name === parameterName)
 		? proposals.find(x => x.name === parameterName).value
-		: null
+    : null
 
 export function * fetchParameters (action) {
   const parameterizer = yield apply(TCR.registry(), 'getParameterizer');
@@ -29,52 +56,26 @@ export function * fetchParameters (action) {
 		value: p.returnValues.value,
   }))
 
-  const params = [
-    {
-      displayName: keys.tableParameterNames[0],
-      contractName: keys.contractParameterNames[0],
-      value: yield apply(parameterizer, 'get', [keys.contractParameterNames[0]]),
-      status: keys.inRegistry,
-      proposal: getProposalValue(proposals, keys.contractParameterNames[0])
-    },
-    {
-      displayName: keys.tableParameterNames[1],
-      contractName: keys.contractParameterNames[1],
-      value: yield apply(parameterizer, 'get', [keys.contractParameterNames[1]]),
-      status: keys.inRegistry,
-      proposal: getProposalValue(proposals, keys.contractParameterNames[1])
-    },
-    {
-      displayName: keys.tableParameterNames[2],
-      contractName: keys.contractParameterNames[2],
-      value: yield apply(parameterizer, 'get', [keys.contractParameterNames[2]]),
-      status: keys.inRegistry,
-      proposal: getProposalValue(proposals, keys.contractParameterNames[2])
-    },
-    {
-      displayName: keys.tableParameterNames[3],
-      contractName: keys.contractParameterNames[3],
-      value: yield apply(parameterizer, 'get', [keys.contractParameterNames[3]]),
-      status: keys.inRegistry,
-      proposal: getProposalValue(proposals, keys.contractParameterNames[3])
-    },
-    {
-      displayName: keys.tableParameterNames[4],
-      contractName: keys.contractParameterNames[4],
-      value: yield apply(parameterizer, 'get', [keys.contractParameterNames[4]]),
-      status: keys.inRegistry,
-      proposal: getProposalValue(proposals, keys.contractParameterNames[4])
-    },
-    {
-      displayName: keys.tableParameterNames[5],
-      contractName: keys.contractParameterNames[5],
-      value: yield apply(parameterizer, 'get', [keys.contractParameterNames[5]]),
-      status: keys.inRegistry,
-      proposal: getProposalValue(proposals, keys.contractParameterNames[5])
-    },
-  ];
+  const params = yield paramsList.map(function * (p) {
+    const proposal = getProposalValue(proposals, p.contractName)
+    const value = yield apply(parameterizer, 'get', [p.contractName])
 
-  yield put({type: UPDATE_PARAMETERIZER_INFORMATION, params});
+    let status = keys.inRegistry
+
+    if (proposal) {
+      const proposalInstance = yield apply(parameterizer, 'getProposal', [p.contractName, proposal])
+      status = yield apply(proposalInstance, 'getStageStatus')
+    }
+
+    return {
+      ...p,
+      proposal,
+      status,
+      value,
+    } 
+  })
+
+  yield put({ type: UPDATE_PARAMETERIZER_INFORMATION, params });
 }
 
 export function* proposeNewParameterizerValue(action) {
