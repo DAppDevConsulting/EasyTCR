8d708e0e7c6bee2d5a2eff1fb90ce47565bd714b
Sergey Kovbasyuk <sergey.kovbasyuk@onereach.com>
2018-03-01 13:49:56 +0200

    added Need process case to parameterizer

--- src/components/ParameterizerAction/index.js
+++ src/components/ParameterizerAction/index.js
@@ -4,6 +4,7 @@ import './style.css';
 import ParameterizerEdit from './ParameterizerEdit';
 import ParameterizerVote from './ParameterizerVote';
 import ParameterizerReveal from './ParameterizerReveal';
+import ParameterizerNeedProcess from './ParameterizerNeedProcess';
 import keys from '../../i18n';
 
 const ParameterizerAction = ({ activeProposal }) => {
@@ -22,9 +23,12 @@ const ParameterizerAction = ({ activeProposal }) => {
 			return <ParameterizerVote activeProposal={activeProposal} />
 		case keys.VoteReveal:
 			return <ParameterizerReveal activeProposal={activeProposal} />
-		// todo: process case
+		case keys.NeedProcess:
+			return <ParameterizerNeedProcess activeProposal={activeProposal} />
 		default:
-			return <p>Unhandled status</p>
+			return <div className="parameterizerAction">
+					<p className="parameterizerNote">Unhandled status</p>
+				</div>
 	}
 };
 
