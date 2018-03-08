75b185ac52f3523245066f302d690d79f63c6038
Alexey Hodkov <alexey.hodkov@gmail.com>
2018-03-08 13:40:04 +0200

    fix parametrizer status

--- src/services/ParametrizerMapper.js
+++ src/services/ParametrizerMapper.js
@@ -3,6 +3,7 @@ import keys from '../i18n';
 
 const map = async (name, localization, parametrizer, proposals) => {
   const value = await parametrizer.get(name);
+  console.log('proposals', proposals);
   const proposalValueFromContract = proposals[name];
   // new proposal cannot be the same as current value
   const proposal = value !== proposalValueFromContract ? proposalValueFromContract : null;
@@ -19,19 +20,22 @@ const map = async (name, localization, parametrizer, proposals) => {
   if (proposal) {
     // get status & challengeId
     const proposalInstance = await parametrizer.getProposal(name, proposal);
-    const statusFromContract = await proposalInstance.getStageStatus();
-    status = getReadableStatus(statusFromContract);
-    challengeId = await proposalInstance.getChallengeId();
-    timestamp = await proposalInstance.expiresAt();
-    timestamp *= 1000;
+    const exists = await proposalInstance.exists();
+    if (exists) {
+      const statusFromContract = await proposalInstance.getStageStatus();
+      status = getReadableStatus(statusFromContract);
+      challengeId = await proposalInstance.getChallengeId();
+      timestamp = await proposalInstance.expiresAt();
+      timestamp *= 1000;
 
-    // get vote results
-    const plcr = await parametrizer.getPLCRVoting();
-    const poll = await plcr.getPoll(challengeId);
-    voteResults = {
-      votesFor: await poll.getVotesFor(),
-      votesAgaints: await poll.getVotesAgainst()
-    };
+      // get vote results
+      const plcr = await parametrizer.getPLCRVoting();
+      const poll = await plcr.getPoll(challengeId);
+      voteResults = {
+        votesFor: await poll.getVotesFor(),
+        votesAgaints: await poll.getVotesAgainst()
+      };
+    }
   }
 
   return { displayName: localization, contractName: name, proposal, status, value, challengeId, voteResults, timestamp };
